8ba03cf9dc50111508d82430b716964b2ba65224
Raghesh <raghesh.a@gmail.com>
2011-11-11 22:39:52 +0530

    Removed unwanted code from week1/1.sh

--- week1/1.sh
+++ week1/1.sh
@@ -7,12 +7,3 @@ then
 else
   echo "NO"
 fi
-# Modify the script in the previous question, to remove the execute permissions,
-# if the file is executable.
-if [ -x $1 ]
-then
-  echo "YES"
-  chmod -x $1
-else
-  echo "NO"
-fi
