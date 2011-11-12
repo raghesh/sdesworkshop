32f756b852aa9e530e381cd5e19c145f90a77731
Raghesh <raghesh.a@gmail.com>
2011-11-12 23:07:39 +0530

    Updating first test script
    Directories are not counted

--- week2/test1/1.sh
+++ week2/test1/1.sh
@@ -1,4 +1,11 @@
-# Write a bash script(1.sh) to count number of files(not hidden files) inside a
-# directory when the directory path is given as argument.
-
-ls $1 | wc -l
+# Write a bash script(1.sh) to count number of regular files(not hidden files)
+# inside a directory when the directory path is given as argument. 
+COUNT=0
+for file in `ls $1`
+do
+  if [ -f $1/$file ]
+  then
+    COUNT=`expr $COUNT + 1`
+  fi
+done
+echo $COUNT
